2d5c7676f2eecbf11f26ff0e81d45c26216fa298
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2019-08-16 01:01:44 +0700

    Added MvcLightweight

---
+++ README.md
@@ -0,0 +1,5 @@
+# aspcore-authentication
+My study project about all authentication.
+
+### List of project
+1. MvcLightweight. It's about cookie authentication without ASP.NET Core Identity.
---
+++ src/MvcLightweight/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcLightweight.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Login()
+        {
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, "chen2584"));
+            identity.AddClaim(new Claim(ClaimTypes.GivenName, "Chen"));
+            identity.AddClaim(new Claim(ClaimTypes.Surname, "Semapat"));
+
+            identity.AddClaim(new Claim(ClaimTypes.Role, "Administrator"));
+
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
